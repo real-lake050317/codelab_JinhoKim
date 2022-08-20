9cae075cc0a1a7437e2e8d38e86e438a7a1447cc
Jinho Kim <79720217+real-lake050317@users.noreply.github.com>
2022-08-21 01:58:49 +0900

    .

---
+++ solvings/9656.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+
+    if (N%2 == 1) {
+        cout << "CY" << endl;
+        
+    }
+    else {
+        cout << "SK" << endl;
+    }
+}
