6ca85bd71fd949a8b58e768268e03f2c16f2203d
Jinho Kim <79720217+real-lake050317@users.noreply.github.com>
2022-08-22 10:53:58 +0900

    Added 15353.cpp

--- solvings/15353.cpp
+++ solvings/15353.cpp
@@ -6,16 +6,20 @@ using namespace std;
 
 int main() {
     char a[10010], b[10010];
+
     stack<int> anum;
     stack<int> bnum;
     stack<int> dp;
+
     bool flag = 0;
+    
     cin >> a >> b;
     
     for (int i = 0; i<strlen(a); i++) {
         int aint = (int)a[i] - 48;
         anum.push(aint);
     }
+
     for (int i = 0; i<strlen(b); i++) {
         int bint = int(b[i]) - 48;
         bnum.push(bint);
@@ -34,9 +38,41 @@ int main() {
         anum.pop();
         bnum.pop();
     }
-    for (int i = 0; i<dp.size(); i++) {
+
+    while (!anum.empty()) {
+        int aint = anum.top();
+        if (aint + flag >= 10) {
+            dp.push((aint + flag)%10);
+            flag = 1;
+        }
+        else {
+            dp.push((aint + flag)%10);
+            flag = 0;
+        }
+        anum.pop();
+    }
+
+    while (!bnum.empty()) {
+        int bint = bnum.top();
+        if (bint + flag >= 10) {
+            dp.push((bint + flag) % 10);
+            flag = 1;
+        }
+        else {
+            dp.push((bint + flag)%10);
+            flag = 0;
+        }
+        bnum.pop();
+    }
+
+    if (flag) {
+        dp.push(1);
+    }
+
+    while (!dp.empty()) {
         int a = dp.top();
         cout << a;
         dp.pop();
     }
+    return 0;
 }
