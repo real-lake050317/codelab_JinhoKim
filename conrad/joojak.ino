1150cf6f138931850bfe10a7dc6ae584f5539941
Jinho Kim <79720217+real-lake050317@users.noreply.github.com>
2023-01-13 23:11:20 +0900

    .

---
+++ conrad/joojak.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+const int firstServoPin = 9;
+const int secondServoPin = 10;
+
+Servo firstServo;
+Servo secondServo;
+
+int firstAngle = 0;
+int secondAngle = 0;
+
+int phi = 0;
+int theta = 0;
+
+void setup() {
+    Serial.begin(9600);
+    firstServo.attach(firstServoPin);
+    secondServo.attach(secondServoPin);
+}
+
+void loop() {
+    firstServo.write(firstAngle);
+    secondServo.write(secondAngle);
+    delay(1000);
+    
+    Serial.println("Configuration finished.")
+
+    firstServo.write(phi);
+    secondServo.write(theta);
+
+    delay(1000);
+
+    Serial.println("Execution Finished");
+
+    exit(0);
+}
